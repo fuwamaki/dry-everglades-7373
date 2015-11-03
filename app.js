b17f0a6a3e42c7126c58298c270461955f2a7d6f
fuwamaki0324 <variety.fuwa.3@gmail.com>
2015-11-03 23:14:27 +0900

    change index

--- app.js
+++ app.js
@@ -1,33 +1,3 @@
-var WebSocketServer = require("ws").Server
-var http = require("http")
-var express = require("express")
-var app = express()
-var port = process.env.PORT || 5000
-
-app.use(express.static("nodejs_chat/"))
-
-var server = http.createServer(app)
-server.listen(port)
-
-console.log("http server listening on %d", port)
-
-var wss = new WebSocketServer({server: server})
-console.log("websocket server created")
-
-wss.on("connection", function(ws) {
-  var id = setInterval(function() {
-    ws.send(JSON.stringify(new Date()), function() {  })
-  }, 1000)
-
-  console.log("websocket connection open")
-
-  ws.on("close", function() {
-    console.log("websocket connection close")
-    clearInterval(id)
-  })
-})
-
-/*
 //httpを読み込む
 var http = require('http');
 //フレームワークexpressを読み込む
@@ -96,4 +66,3 @@ io.sockets.on('connection', function (socket) {
         
 
 });
-*/
