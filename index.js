c074154fd3cf83a0694fdcf5941698fc5f88bc2a
fuwamaki0324 <variety.fuwa.3@gmail.com>
2015-11-09 18:16:41 +0900

    change index.js,html

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//それぞれ変数の定義
 var WebSocketServer = require("ws").Server
 var http = require("http")
 var express = require("express")
@@ -9,33 +10,40 @@ app.use(express.static(__dirname + "/"))
 var server = http.createServer(app)
 server.listen(port)
 
+//consoleに表示
+//※ポート番号は毎回変わる
 console.log("http server listening on %d", port)
 
+//ポートにWebSocketサーバを立てる
 var wss = new WebSocketServer({server: server})
 console.log("websocket server created")
 
 //Websocket接続を保存しておく
 var connections = [];
 
+//クライアントと接続すると動作するイベント
 wss.on("connection", function(ws) {
+
   var id = setInterval(function() {
-    ws.send(JSON.stringify(new Date()), function() {  })
+  //接続時のメッセージ
+  ws.send(JSON.stringify(new Date()), function() {  })
   }, 1000)
 
   console.log("websocket connection open")
 
+  //websocketがクローズした際のメッセージ
   ws.on("close", function() {
     console.log("websocket connection close")
     clearInterval(id)
   })
   
       //配列にWebSocket接続を保存
-    connections.push(ws);
+  connections.push(ws);
 
       //メッセージ送信時
-    ws.on('message', function (message) {
-        console.log('message:', message);
-        broadcast(JSON.stringify(message));
+  ws.on('message', function (message) {
+    console.log('message:', message);
+    broadcast(JSON.stringify(message));
     });
 })
 
