a76c12c22c7132510a7b6ed94e2fd086ba871445
fuwamaki0324 <variety.fuwa.3@gmail.com>
2015-11-11 23:55:03 +0900

    change html,js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ console.log("コンソール：websocket server created");
 
 //クライアントと接続すると動作するイベント
 wss.on("connection", function(ws) {
-//	失敗console.log("IDナンバー:" + ws.id);
+//	失敗した console.log("IDナンバー:" + ws.id);
 
 	//接続時のメッセージ
 	var id = setInterval(function() {
