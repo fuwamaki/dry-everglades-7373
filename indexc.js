c0dd1259b76abd40bfb6fafc28d41cede0f1c8f6
fuwamaki0324 <variety.fuwa.3@gmail.com>
2015-11-27 03:17:56 +0900

    11月27日

--- indexc.js
+++ indexc.js
@@ -43,7 +43,7 @@ window.onunload = function(event){
 ws.onmessage = function (event) {
 	//-----メッセージの代入-----
 	var messages = JSON.parse(event.data);
-//	console.log(messages);
+	console.log(messages);
 
 	//-----typeがping-----
 	if(messages.type == "ping"){
