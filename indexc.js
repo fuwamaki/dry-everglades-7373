f9308fd827e953f3a42ea0e9d2fe2983146d8ae5
fuwamaki0324 <variety.fuwa.3@gmail.com>
2015-12-07 04:54:03 +0900

    12月7日

--- indexc.js
+++ indexc.js
@@ -448,8 +448,6 @@ function onScriptDecideButton(){
 	trainingsend('0', 'Tablet2','training_prepare',scriptArray[1][1],scriptArray[1][2],scriptArray[1][3]);
 	trainingsend('0', 'Tablet3','training_prepare',scriptArray[2][1],scriptArray[2][2],scriptArray[2][3]);
 	
-	//常にcountを送り続けてみる
-	var id = setInterval(send("ping1",'training_send', count), 1000);
 }
 
 //+++++稽古操作部分+++++
